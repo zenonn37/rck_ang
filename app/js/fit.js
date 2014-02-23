22b3e610641be9485a040bc2041ff24115d90525
chris crowd <zent29@gmail.com>
2014-02-23 16:58:28 -0500

    good night

---
+++ app/js/fit.js
@@ -0,0 +1,16 @@
+/**
+ * Created by pure coder on 2/23/14.
+ */
+
+
+
+
+
+$(document).ready(function(){
+
+
+    jQuery("#responsive_headline").fitText();
+
+    jQuery("#responsive").fitText(1.2, { minFontSize: '10px', maxFontSize: '24px' });
+
+});
--- app/js/pages-controller.js
+++ app/js/pages-controller.js
@@ -6,6 +6,23 @@ app.controller('PagesController',function($scope){
 
  $scope.header = "Welcome to RC Kinectics";
 
+    $scope.racing = "RCK offers a 15,000 square foot facility with an asphalt and dirt track.";
+
+    $scope.pro = "New RC Cars and Trucks, Parts, Accessories, Repairs, and rentals.";
+
+    $scope.event = "Let Us Host Your Birthday Parties, and Business Events.";
+
+
+
+    //console.log($scope.event.length);
+
+
+   $scope.onViewLoad = function(){
+
+      console.log("view changed");
+
+   }
+
 });
 
 
